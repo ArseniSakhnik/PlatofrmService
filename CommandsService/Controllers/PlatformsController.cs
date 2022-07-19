928d6b4c8220a82ac469c2a91dcc7e9af8ababe4
sakhn <sakhnikarseni@mail.ru>
2022-07-19 21:16:39 +0300

    да

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controller");
+        } 
+    }
+}
