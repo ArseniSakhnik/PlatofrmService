c6fe31cd4103ca2471095b406659f49cecfb59ac
sakhn <sakhnikarseni@mail.ru>
2022-07-24 22:11:42 +0300

    5ч 7м

--- k8s/run.cmd
+++ k8s/run.cmd
@@ -2,3 +2,6 @@
 kubectl apply -f platforms-np-srv.yaml
 kubectl get services
 kubectl get deployments
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/aws/deploy.yaml
+kubectl get pods --namespace=ingress-nginx
+kubectl delete all --all
